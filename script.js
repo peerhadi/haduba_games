fcc10470645b94d1916fce2b3dfebd837d7abc91
peerhadi <80880103+peerhadi@users.noreply.github.com>
2023-03-13 14:19:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,190 +1,165 @@
-window.addEventListener("load",function(){
-    const canvas = document.getElementById("canvas1");
-    const ctx = canvas.getContext("2d");
-    canvas.width = 1280;
-    canvas.height = 720;
-
-    class InputHandler {
-        constructor(game){
-            this.game = game;
-            window.addEventListener("keydown", e => {
-                this.game.lastKey = "P" + e.key
-            })
-            window.addEventListener("keyup", e => {
-                this.game.lastKey = "R" + e.key;
-            })
-        }
+class AudioController{
+    constructor(){
+        this.bgMusic = new Audio("/Users/hadi/Desktop/Assets_Audio_creepy.mp3 ");
+        this.flipSound = new Audio("/Users/hadi/Desktop/Assets_Audio_flip.wav ");
+        this.matchSound = new Audio("/Users/hadi/Desktop/Assets_Audio_match.wav ");
+        this.victorySound = new Audio("/Users/hadi/Desktop/Assets_Audio_victory.wav ");
+        this.gameOverSound = new Audio("/Users/hadi/Desktop/Assets_Audio_gameOver.wav ");
+        this.bgMusic.volumne = 0.2;
+        this.bgMusic.loop = true;
     }
-
-    class Owlbear {
-        constructor(game){
-            this.game = game;
-            this.spriteWidth = 200;
-            this.spriteHeight = 200;
-            this.frameX = 0;
-            this.frameY = 0;
-            this.maxFrame = 30;
-            this.width = this.spriteHeight;
-            this.height = this.spriteWidth;
-            this.x = 200;
-            this.y = 200;
-            this.speedX = 0;
-            this.speedY = 0;
-            this.maxSpeed = 5; 
-            this.image = document.getElementById("owlbear");
-            this.fps = 100;
-            this.frameInterval = 1000/this.fps;
-            this.frameTimer = 0;
-        }
-        draw(context){
-            context.drawImage(this.image,this.frameX * this.spriteWidth,this.frameY * this.spriteHeight,this.spriteWidth,this.spriteHeight,this.x,this.y,this.width, this.height);
-        }
-        setSpeed(speedX,speedY){
-            this.speedX = speedX;
-            this.speedY = speedY;
-        }
-        update(deltaTime){
-            if(this.game.lastKey == "PArrowLeft"){
-                this.setSpeed(-this.maxSpeed, 0);
-                this.frameY = 3;
-            }else if(this.game.lastKey == "RArrowLeft" && this.speedX < 0){
-                this.setSpeed(0, 0);
-                this.frameY = 2;
-            } else if(this.game.lastKey == "PArrowRight"){ 
-                this.setSpeed(this.maxSpeed,0);
-                this.frameY = 5;
-            } else if(this.game.lastKey == "RArrowRight"){
-                this.setSpeed(0, 0);
-                this.frameY = 4;
-            }else if(this.game.lastKey == "PArrowUp"){
-                this.setSpeed(0, -this.maxSpeed * 0.6);
-                this.frameY = 7;
-            } else if((this.game.lastKey == "RArrowUp") && this.speedY < 0){
-                this.setSpeed(0, 0);
-                this.frameY = 6;
-            } else if(this.game.lastKey == "PArrowDown"){
-                this.setSpeed(0, this.maxSpeed * 0.6);
-                this.frameY = 1;
-            }else if((this.game.lastKey == "RArrowDown") && this.speedY > 0){
-                this.setSpeed(0, 0);
-                this.frameY = 0;
-            }
-            this.x += this.speedX;
-            this.y += this.speedY;
-
-            if(this.x < 0){
-                this.x = 0;
-            }else if(this.x > this.game.width - this.width){
-                this.x = this.game.width - this.width;
-            }else if(this.y < 0 + this.game.topMargin){
-                this.y = this.game.topMargin;
-            }else if(this.y > this.game.height - this.height){
-                this.y = this.y = this.game.height - this.height;
-            }
-            if(this.frameTimer > this.frameInterval){
-                this.frame = this.frameX < this.maxFrame ? this.frameX++ : this.frameX = 0;
-            }else{
-                this.frameTimer += deltaTime;
-            }
-        }
+    startMusic(){
+        this.bgMusic.play();
     }
-
-    class Object{
-        constructor(game){
-            this.game = game;
-        }
-        draw(context){
-            context.drawImage(this.image, this.x, this.y, this.width, this.height);
-        }
-        update(){
-            
-        }
+    stopMusic(){
+        this.bgMusic.pause();
+        this.bgMusic.AudioControllercurentTime = 0;
     }
-        class Bush extends Object {
-            constructor(game){
-                super(game);
-                this.game = game;
-                this.image = document.getElementById("bush")
-                this.imageWidth = 216;
-                this.imageHeight = 100;
-                this.width = this.imageWidth;
-                this.height = this.imageHeight;
-                this.x = Math.random() * this.game.width - this.width;
-                this.y = this.game.topMargin + Math.random() * (this.game.height - this.height - this.game.topMargin)
-            }
-        }
-        class Plant extends Object{
-            constructor(){
-                super(game);
-                this.game = game;
-                this.image = document.getElementById("plant")
-                this.imageWidth = 212;
-                this.imageHeight = 118;
-                this.width = this.imageWidth;
-                this.height = this.imageHeight;
-                this.x = Math.random() * this.game.width - this.width;
-                this.y = this.game.topMargin + Math.random() * (this.game.height - this.height - this.game.topMargin)
-            }
-        }
-        class Grass extends Object{
-            constructor(game){
-                super(game);
-                this.game = game;
-                this.image = document.getElementById("grass")
-                this.imageWidth = 103;
-                this.imageHeight = 182;
-                this.width = this.imageWidth;
-                this.height = this.imageHeight;
-                this.x = Math.random()  * this.game.width - this.width;
-                this.y = this.game.topMargin + Math.random() * (this.game.height - this.height - this.game.topMargin)
-            }
-        }
-    class Game{
-        constructor(width,height){
-            this.width = width;
-            this.height = height;
-            this.lastKey = undefined;
-            this.input = new InputHandler(this);
-            this.owlbear = new Owlbear(this);
-            this.topMargin = 200;
-            this.numberOfPlants = 10;
-            this.plants = [];
-            this.gameObjects = [];
-        }
-        render(context,deltaTime){
-            this.gameObjects = [...this.plants,this.owlbear];
-            this.gameObjects.sort((a,b) => {
-                return (a.y + a.height) - (b.y - b.height);
-            });
-            this.gameObjects.forEach(object => {
-                object.draw(context);
-                object.update(deltaTime);
-            })
-        }
-        init(){
-            for(let i = 0; i < this.numberOfPlants; i++){
-                const randomize = Math.random();
-                if(randomize < 0.3){
-                    this.plants.push(new Bush(this));
-                }else if(randomize < 0.6){
-                    this.plants.push(new Plant(this))
-                }else{
-                    this.plants.push(new Grass(this))
-                }
-            }
-        }
+    flip(){
+        this.flipSound.play();
     }
-     
-    const game = new Game(canvas.width,canvas.height);
-    game.init()
-    let lastTime = 0;
-    function animate(timeStamp){
-        const deltaTime = timeStamp - lastTime;
-        lastTime = timeStamp;
+    match(){
+        this.matchSound.play();
+    }
+    victory(){
+        this.stopMusic();
+        this.victorySound.play();
+    }
+    gameOver(){
+        this.stopMusic();
+        this.gameOverSound.play();
+    }
+}
+class MixOrMatch{
+    constructor(){
+    }
+    startGame(totalTime,cards){
+        this.timeRemaining = totalTime;
+        this.timer = document.getElementById("time-remaining");
+        this.ticker = document.getElementById("flips");
+        this.audioController = new AudioController(); 
+        this.cardToCheck = null;
+        this.totalClicks = 0;
+        this.matchedCards = [];
+        this.busy = true;
         
-        ctx.clearRect(0,0, canvas.width, canvas.height);
-        game.render(ctx,deltaTime);
-        requestAnimationFrame(animate);
+        setTimeout(() => {
+            this.audioController.startMusic();
+            this.shuffleCards(cards);
+            this.countDown = this.startCountDown();
+            this.busy = false
+        },500);
+        this.hideCards(cards);
+        this.timer.innerText = this.timeRemaining;
+        this.ticker.innerHTML = this.totalClicks;
     }
-    animate(0); 
-})
+
+    hideCards(cards){
+        cards.forEach(card => {
+            card.classList.remove("visible");
+            card.classList.remove("matched");
+        })
+    }
+    
+    flipCard(card,cards){
+        if(this.canFlipCard(card)){
+            this.audioController.flip();
+            this.totalClicks++;
+            this.ticker.innerHTML = this.totalClicks;
+            card.classList.add("visible")
+
+            if(this.cardToCheck)
+                this.checkForCardMatch(card,cards)
+                else
+                    this.cardToCheck = card
+        }
+    }
+
+    checkForCardMatch(card,cards){
+        if(this.getCardType(card) === this.getCardType(this.cardToCheck)){
+            this.cardMatch(card,this.cardToCheck,cards)
+        }else{
+            this.cardMisMatch(card,this.cardToCheck)
+            this.cardToCheck = null;
+        }
+    }
+    cardMatch(card1,card2,cards){
+        this.matchedCards.push(card1);
+        this.matchedCards.push(card2);
+        card1.classList.add("matched")
+        card2.classList.add("matched")
+        this.cardToCheck = null;
+        this.audioController.match();
+        if(this.matchedCards.length === cards.length){
+            this.victory();  
+        }
+    }
+    
+    cardMisMatch(card1,card2){
+        this.busy = true;
+        setTimeout(() => {
+            card1.classList.remove("visible")
+            card2.classList.remove("visible")
+            this.busy = false;
+        }, 1000)
+    }
+    
+    getCardType(card){
+        return card.getElementsByClassName("card-value")[0].src;
+    }
+    startCountDown(){
+        return setInterval(() => {
+            this.timeRemaining--;
+            this.timer.innerText = this.timeRemaining;
+            if(this.timeRemaining === 0)
+                this.gameOver();
+        }, 1000)
+    }
+    
+    gameOver(){
+        clearInterval(this.countDown);
+        this.audioController.gameOver();
+        document.getElementById("game-over-text").classList.add("visible");
+    }
+    victory(){
+        clearInterval(this.countDown);
+        this.audioController.victory();
+        document.getElementById("victory-text").classList.add("visible");
+    }
+    shuffleCards(cards){
+        for(let i = cards.length - 1; i > 0; i--){
+            let randomIndex = Math.floor(Math.random() * (i + 1))
+            cards[randomIndex].style.order = i;
+            cards[i].style.order = randomIndex;
+        }
+    }
+    
+    canFlipCard(card){
+        return (!this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck );
+    }
+}
+function ready(){
+    let overlays = Array.from(document.getElementsByClassName("overlay-text"));
+    let cards = Array.from(document.getElementsByClassName("card"));
+    let game = new MixOrMatch(); 
+    overlays.forEach(overlay => {
+        overlay.addEventListener("click", () => {
+            overlay.classList.remove("visible")
+            game.startGame(100,cards);
+        })
+    })
+
+    cards.forEach(card => {
+        card.addEventListener("click", () => {
+            game.flipCard(card,cards);
+        });
+    })
+}
+
+if(document.readyState === "loading"){
+    document.addEventListener("DOMContentLoaded", ready());
+} else {
+    ready();
+}
+
+
